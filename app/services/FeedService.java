6228c3c73d1e9d5c7bb8803acf01e50fa0ce0399
Live session user <vishwalpk3@gmail.com>
2017-09-15 06:18:29 +0000

    Third Commit

---
+++ app/services/FeedService.java
@@ -0,0 +1,45 @@
+package services;
+
+import data.FeedResponse;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import play.libs.ws.WS;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+
+//import javax.swing.text.Document;
+//import javax.xml.soap.Node;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+public class FeedService {
+
+    public FeedResponse getFeedResponse ( String keyword ) throws ExecutionException, InterruptedException{
+
+        FeedResponse feedResponseObject =  new FeedResponse();
+
+        try{
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q", keyword)
+                    .setQueryParameter("output", "rss")
+                    .get();
+
+            Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+
+            Node item = feedResponse.getFirstChild().getFirstChild().getChildNodes().item(9);
+            feedResponseObject.title = item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponseObject.pubDate = item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedResponseObject.description = item.getChildNodes().item(4).getFirstChild().getNodeValue();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return feedResponseObject;
+    }
+
+
+
+}
