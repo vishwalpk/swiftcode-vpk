71fd2d907d75364b0a3990870e26e0a0dca930b3
Live session user <vishwalpk3@gmail.com>
2017-09-16 13:16:42 +0000

    Working Login

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,9 +1,20 @@
+
 package controllers;
-
 import actors.MessageActor;
-import play.mvc.*;
+import data.LoginForm;
+import play.Configuration;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.LegacyWebSocket;
+import play.mvc.Result;
+import play.mvc.Results;
+import play.mvc.WebSocket;
+import views.html.index;
+import views.html.login;
+import javax.inject.Inject;
+import java.util.Objects;
 
-import views.html.*;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -11,6 +22,12 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
+
+    @Inject
+   Configuration configuration;
+
+   @Inject
+   FormFactory formFactory;
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -20,6 +37,17 @@ public class HomeController extends Controller {
     public Result index() {
         return ok(views.html.index.render());
     }
+
+       public Result login() {
+       return ok(views.html.login.render(""));
+   }    public Result authenticate(){
+       Form<LoginForm> loginFormForm = formFactory.form(LoginForm.class).bindFromRequest();
+       if(Objects.equals(configuration.getString("app.user.username"), loginFormForm.get().getUsername()) &&
+               Objects.equals(configuration.getString("app.user.password"), loginFormForm.get().getPassword())){
+           return ok();
+       }
+       return Results.unauthorized();
+   }
 
     public LegacyWebSocket<String> chatSocket(){
         return WebSocket.withActor(MessageActor::props);
